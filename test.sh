e4194dbe910a9d42b49145e6af7e65a34cd3a0d6
shafi <mehedishafi@hotmail.com>
2018-10-20 22:27:49 +0600

    added eclipse installer.

--- test.sh
+++ test.sh
@@ -10,21 +10,21 @@ check_command()
 
 # Update index
 sudo apt-get update >> log.txt
-check_command "Index Update Success" "Update Failed"
+check_command "Index Update Success" ""
 
 # install G++
 sudo apt install g++ -y >> log.txt
-check_command "C++ compiler Installed" "C++ installation failed"
+check_command "C++ compiler Installed" ""
 
 # Install CodeBlocks
 sudo apt install codeblocks -y >> log.txt
-check_command "CodeBlocks installed" "CodeBlocks Installed Failed"
+check_command "CodeBlocks installed" ""
 
 # install java
 sudo apt install default-jre -y >> log.txt
-check_command "JRE Installed" "JRE installation failed"
+check_command "JRE Installed" ""
 sudo apt install default-jdk -y >> log.txt
-check_command "JDK Installed" "JDK installation failed"
+check_command "JDK Installed" ""
 
 # test java installation
 java -version
@@ -32,23 +32,29 @@ javac -version
 
 # install python
 sudo apt install python-minimal >> log.txt
-check_command "Python 2 installed" "Python installation failed"
+check_command "Python 2 installed" ""
 
 # install sublime
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-add-repository "deb https://download.sublimetext.com/ apt/stable/" 
 sudo apt update >> log.txt
 sudo apt install sublime-text -y >> log.txt
-check_command "Sublime installed" "Sublime installation failed"
+check_command "Sublime installed" ""
 
 # install kate
 sudo apt install kate -y >> log.txt
-check_command "Kate installed" "Kate installation failed"
+check_command "Kate installed" ""
 
 # install chrome
 # sudo apt install gdebi-core -y >> log.txt
 # wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 # sudo gdebi google-chrome-stable_current_amd64.deb
+
+
+# install eclipse
+cp eclipse.tar.gz ~/Downloads
+tar xfz ~/Downloads/eclipse.tar.gz
+~/Downloads/eclipse/eclipse-installer/eclipse-inst
 
 mkdir output
 
